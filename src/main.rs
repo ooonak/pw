00dcd087d65f2da173b83e0cd870c2451fe70c23
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2024-10-01 22:04:05 +0200

    Added boot id and serialization.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,27 @@
 use std::fs;
+use serde::{Deserialize, Serialize};
+use rmp_serde::{Deserializer, Serializer};
+
+#[derive(Debug, PartialEq, Deserialize, Serialize)]
+struct Machine {
+    boot_id: String,
+}   
 
 fn boot_id() -> String {
-    let contents = fs::read_to_string("/proc/sys/kernel/random/boot_id").unwrap_or_default();
+    let mut contents = fs::read_to_string("/proc/sys/kernel/random/boot_id").unwrap_or_default();
+    if contents.ends_with('\n') {
+        contents.pop();
+    }
+
     contents
 }
 
 fn main() {
-    println!("boot_id: {}", boot_id());
+    let machine = Machine { boot_id: boot_id().into() };
+    
+    let mut buf = Vec::new();
+    machine.serialize(&mut Serializer::new(&mut buf)).unwrap();
+
+    println!("{:?}", machine);
+    println!("MessagePack ({} bytes) {:x?}", buf.len(), buf);
 }
