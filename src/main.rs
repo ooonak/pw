d9905b6b51ea73945321911585e7a55ffe4fb571
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2024-10-01 22:46:36 +0200

    Added MAC address of of first non-localhost iface as machine id.

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,32 @@ use std::fs;
 use serde::{Deserialize, Serialize};
 use rmp_serde::{Deserializer, Serializer};
 
+use mac_address::{get_mac_address, MacAddress};
+
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 struct Machine {
-    boot_id: String,
+    mac: [u8; 6],
+    boot: String,
 }   
 
-fn boot_id() -> String {
+fn identity() -> Machine {
+    let mut mac = [0, 0, 0, 0, 0, 0];
+
+    match get_mac_address() {
+        Ok(Some(ma)) => { mac = ma.bytes(); }
+        _ => {}
+    }
+
     let mut contents = fs::read_to_string("/proc/sys/kernel/random/boot_id").unwrap_or_default();
     if contents.ends_with('\n') {
         contents.pop();
     }
 
-    contents
+    Machine { mac: mac, boot: contents }
 }
 
 fn main() {
-    let machine = Machine { boot_id: boot_id().into() };
+    let machine = identity();
     
     let mut buf = Vec::new();
     machine.serialize(&mut Serializer::new(&mut buf)).unwrap();
