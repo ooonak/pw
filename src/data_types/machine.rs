40b0e6abdedf539a0873fc8b3b2d2aa610c5ef04
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2024-10-19 13:22:40 +0200

    Added mac and ipv4 for first network interface that is also used for default route.

--- src/data_types/machine.rs
+++ src/data_types/machine.rs
@@ -1,8 +1,8 @@
-use std::io;
-use serde::{Deserialize, Serialize};
-use rmp_serde::Serializer;
-use crate::data_types::{traits, utils};
 use super::utils::{find_default_dev, find_iface_info, parse_lines, read_lines};
+use crate::data_types::{traits, utils};
+use rmp_serde::Serializer;
+use serde::{Deserialize, Serialize};
+use std::io;
 
 #[derive(Debug, Default, PartialEq, Deserialize, Serialize)]
 pub struct Machine {
@@ -15,7 +15,7 @@ pub struct Machine {
     pub meminfo: Vec<String>,
 }
 
-impl Machine {    
+impl Machine {
     pub fn new() -> Self {
         Default::default()
     }
@@ -37,25 +37,29 @@ impl Machine {
                 return info.unwrap();
             }
         }
-        
-        (0,0)
+
+        (0, 0)
     }
 
     fn parse_uptime() -> u32 {
         let lines = read_lines("/proc/uptime").expect("Could not read /proc/uptime");
         if !lines.is_empty() {
             // /proc/uptime contains '2747.41 17969.77', where first number is seconds since boot.
-            let floats = lines[0].split(" ").filter_map(|s| s.parse::<f32>().ok()).collect::<Vec<_>>();
+            let floats = lines[0]
+                .split(" ")
+                .filter_map(|s| s.parse::<f32>().ok())
+                .collect::<Vec<_>>();
             if !floats.is_empty() {
                 return floats[0] as u32;
-            }   
+            }
         }
-        
+
         0
     }
 
     fn parse_boot_id() -> String {
-        let lines = read_lines("/proc/sys/kernel/random/boot_id").expect("Could not read /proc/sys/kernel/random/boot_id");
+        let lines = read_lines("/proc/sys/kernel/random/boot_id")
+            .expect("Could not read /proc/sys/kernel/random/boot_id");
         parse_lines(lines, vec![]).pop().unwrap_or("".to_string())
     }
 
@@ -66,15 +70,24 @@ impl Machine {
 
     fn parse_cpuinfo() -> Vec<String> {
         let lines = read_lines("/proc/cpuinfo").expect("Could not read /proc/cpuinfo");
-        let elements = vec![ ("vendor_id", false), ("model name", false), ("cpu cores", false), ("cpu MHz", false) ];
-        
+        let elements = vec![
+            ("vendor_id", false),
+            ("model name", false),
+            ("cpu cores", false),
+            ("cpu MHz", false),
+        ];
+
         parse_lines(lines, elements)
     }
 
     fn parse_meminfo() -> Vec<String> {
         let lines = read_lines("/proc/meminfo").expect("Could not read /proc/meminfo");
-        let elements = vec![ ("MemTotal:", false), ("MemFree:", false), ("MemAvailable:", false) ];
-        
+        let elements = vec![
+            ("MemTotal:", false),
+            ("MemFree:", false),
+            ("MemAvailable:", false),
+        ];
+
         parse_lines(lines, elements)
     }
 }
@@ -84,7 +97,7 @@ mod tests {
     use std::path::PathBuf;
 
     use super::*;
-    
+
     #[test]
     fn version() {
         let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
@@ -94,17 +107,17 @@ mod tests {
 
         let info = parse_lines(lines, vec![]);
         assert_eq!(info.len(), 1);
-            assert_eq!(info[0], "Linux version 4.14.44-gafd0c90dd7be (jenkins@miro) (gcc version 4.7 (GCC)) #1 SMP Wed Jul 19 11:56:13 CEST 2023");
+        assert_eq!(info[0], "Linux version 4.14.44-gafd0c90dd7be (jenkins@miro) (gcc version 4.7 (GCC)) #1 SMP Wed Jul 19 11:56:13 CEST 2023");
     }
 
     #[test]
     fn cpuinfo() {
         let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         path.push("resources/test/proc/cpuinfo");
-    
+
         let lines = read_lines(path).expect("Could not read");
-        let elements = vec![ ("model name", false) ];
-        
+        let elements = vec![("model name", false)];
+
         let info = parse_lines(lines, elements);
         assert_eq!(info.len(), 1);
         assert_eq!(info[0], "model name : ARMv7 Processor rev 10 (v7l)");
@@ -114,10 +127,14 @@ mod tests {
     fn meminfo() {
         let mut path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         path.push("resources/test/proc/meminfo");
-    
+
         let lines = read_lines(path).expect("Could not read");
-        let elements = vec![ ("MemTotal:", false), ("MemFree:", false), ("MemAvailable:", false) ];
-        
+        let elements = vec![
+            ("MemTotal:", false),
+            ("MemFree:", false),
+            ("MemAvailable:", false),
+        ];
+
         let info = parse_lines(lines, elements);
         assert_eq!(info.len(), 3);
         assert_eq!(info[0], "MemTotal: 990180 kB");
