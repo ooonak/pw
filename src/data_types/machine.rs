23256e5250e20d3bcf3f09f2c01213e762825db5
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2024-11-05 11:05:03 +0100

    refactor: Using protobuf instead of msgpack.

--- src/data_types/machine.rs
+++ src/data_types/machine.rs
@@ -1,92 +1,92 @@
-use super::utils::{find_default_dev, find_iface_info, parse_lines, read_lines};
-use serde::{Deserialize, Serialize};
+use super::{pw, utils::{find_default_dev, find_iface_info, parse_lines, read_lines}};
 
-#[derive(Debug, Default, PartialEq, Deserialize, Serialize)]
-pub struct Machine {
-    pub mac: u64,
-    pub ipv4: u32,
-    pub uptime: u32,
-    pub boot_id: String,
-    pub version: String,
-    pub cpuinfo: Vec<String>,
-    pub meminfo: Vec<String>,
+use prost::Message;
+
+pub fn load() -> pw::Machine {
+    let mut machine = pw::Machine::default();
+    
+    (machine.mac, machine.ipv4) = parse_mac_and_ip();
+    machine.uptime = parse_uptime();
+    machine.bootid = parse_boot_id();
+    machine.version = parse_version();
+    machine.cpuinfo = parse_cpuinfo();
+    machine.meminfo = parse_meminfo();
+
+    machine
 }
 
-impl Machine {
-    pub fn new() -> Self {
-        Default::default()
-    }
+pub fn serialize_machine(machine: &pw::Machine) -> Vec<u8> {
+    let mut buf = Vec::new();
+    buf.reserve(machine.encoded_len());
+    // Unwrap is safe, we have reserved capacity in the vector.
+    machine.encode(&mut buf).unwrap();
+    buf
+}
 
-    pub fn load(&mut self) {
-        (self.mac, self.ipv4) = Machine::parse_mac_and_ip();
-        self.uptime = Machine::parse_uptime();
-        self.boot_id = Machine::parse_boot_id();
-        self.version = Machine::parse_version();
-        self.cpuinfo = Machine::parse_cpuinfo();
-        self.meminfo = Machine::parse_meminfo();
-    }
+pub fn deserialize_machine(buf: &[u8]) -> Result<pw::Machine, prost::DecodeError> {
+    pw::Machine::decode(buf)
+}
 
-    fn parse_mac_and_ip() -> (u64, u32) {
-        let dev = find_default_dev();
-        if dev.is_some() {
-            let info = find_iface_info(&dev.unwrap());
-            if info.is_some() {
-                return info.unwrap();
-            }
+fn parse_mac_and_ip() -> (u64, u32) {
+    let dev = find_default_dev();
+    if dev.is_some() {
+        let info = find_iface_info(&dev.unwrap());
+        if info.is_some() {
+            return info.unwrap();
         }
-
-        (0, 0)
     }
 
-    fn parse_uptime() -> u32 {
-        let lines = read_lines("/proc/uptime").expect("Could not read /proc/uptime");
-        if !lines.is_empty() {
-            // /proc/uptime contains '2747.41 17969.77', where first number is seconds since boot.
-            let floats = lines[0]
-                .split(" ")
-                .filter_map(|s| s.parse::<f32>().ok())
-                .collect::<Vec<_>>();
-            if !floats.is_empty() {
-                return floats[0] as u32;
-            }
+    (0, 0)
+}
+
+fn parse_uptime() -> u32 {
+    let lines = read_lines("/proc/uptime").expect("Could not read /proc/uptime");
+    if !lines.is_empty() {
+        // /proc/uptime contains '2747.41 17969.77', where first number is seconds since boot.
+        let floats = lines[0]
+            .split(" ")
+            .filter_map(|s| s.parse::<f32>().ok())
+            .collect::<Vec<_>>();
+        if !floats.is_empty() {
+            return floats[0] as u32;
         }
-
-        0
     }
 
-    fn parse_boot_id() -> String {
-        let lines = read_lines("/proc/sys/kernel/random/boot_id")
-            .expect("Could not read /proc/sys/kernel/random/boot_id");
-        parse_lines(lines, vec![]).pop().unwrap_or("".to_string())
-    }
+    0
+}
 
-    fn parse_version() -> String {
-        let lines = read_lines("/proc/version").expect("Could not read /proc/version");
-        parse_lines(lines, vec![]).pop().unwrap_or("".to_string())
-    }
+fn parse_boot_id() -> String {
+    let lines = read_lines("/proc/sys/kernel/random/boot_id")
+        .expect("Could not read /proc/sys/kernel/random/boot_id");
+    parse_lines(lines, vec![]).pop().unwrap_or("".to_string())
+}
 
-    fn parse_cpuinfo() -> Vec<String> {
-        let lines = read_lines("/proc/cpuinfo").expect("Could not read /proc/cpuinfo");
-        let elements = vec![
-            ("vendor_id", false),
-            ("model name", false),
-            ("cpu cores", false),
-            ("cpu MHz", false),
-        ];
+fn parse_version() -> String {
+    let lines = read_lines("/proc/version").expect("Could not read /proc/version");
+    parse_lines(lines, vec![]).pop().unwrap_or("".to_string())
+}
 
-        parse_lines(lines, elements)
-    }
+fn parse_cpuinfo() -> Vec<String> {
+    let lines = read_lines("/proc/cpuinfo").expect("Could not read /proc/cpuinfo");
+    let elements = vec![
+        ("vendor_id", false),
+        ("model name", false),
+        ("cpu cores", false),
+        ("cpu MHz", false),
+    ];
 
-    fn parse_meminfo() -> Vec<String> {
-        let lines = read_lines("/proc/meminfo").expect("Could not read /proc/meminfo");
-        let elements = vec![
-            ("MemTotal:", false),
-            ("MemFree:", false),
-            ("MemAvailable:", false),
-        ];
+    parse_lines(lines, elements)
+}
 
-        parse_lines(lines, elements)
-    }
+fn parse_meminfo() -> Vec<String> {
+    let lines = read_lines("/proc/meminfo").expect("Could not read /proc/meminfo");
+    let elements = vec![
+        ("MemTotal:", false),
+        ("MemFree:", false),
+        ("MemAvailable:", false),
+    ];
+
+    parse_lines(lines, elements)
 }
 
 #[cfg(test)]
