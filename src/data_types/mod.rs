7aa2ea2536d6299daba30409993db9a798b2a31d
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2024-11-05 11:26:34 +0100

    fix: Builds again.

--- src/data_types/mod.rs
+++ src/data_types/mod.rs
@@ -2,6 +2,8 @@ pub mod machine;
 
 mod utils;
 
-pub mod pw   {
-    include!(concat!(env!("OUT_DIR"), "/pw.rs"));
+pub mod pw {
+    pub mod messages {
+        include!(concat!(env!("OUT_DIR"), "/pw.messages.rs"));
+    }
 }
