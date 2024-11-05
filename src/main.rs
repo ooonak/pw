a1e57dae63790e1f36236efedb1203d954b7d70a
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2024-11-05 12:41:44 +0100

    feat: Puts machine info via Zenoh.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 mod data_types;
 
-fn main() {
+use data_types::pw;
+use zenoh::{bytes::ZBytes, key_expr::{self, KeyExpr}, Config};
+
+async fn send_machine_info(session: &zenoh::Session, machine: &pw::messages::Machine) {
+    let payload = ZBytes::from(data_types::machine::serialize_machine(&machine));
+
+    let key = format!("pw/machine/{}", machine.mac);
+
+    println!("Putting Data ('{key}': {} bytes)...", payload.len());
+    
+    session.put(&key, payload).await.unwrap();
+}
+
+#[tokio::main]
+async fn main() {
+    // initiate logging
+    zenoh::init_log_from_env_or("error");
+
+    let config = zenoh::Config::default();
+    //config.connect.endpoints.set(["tcp/127.0.0.1:7447"].iter().map(|s|s.parse().unwrap()).collect());
+    
+    println!("Opening session...");
+    let session = zenoh::open(config).await.unwrap();
+
     let machine = data_types::machine::load();
+    send_machine_info(&session, &machine).await;
 
-    let buffer = data_types::machine::serialize_machine(&machine);
-
-    println!("Serialized, buffer contains: {:?}", buffer);
-
+    //println!("Serialized, buffer contains: {:?}", buffer);
+    /*
     let deserialized = data_types::machine::deserialize_machine(&buffer);
     println!("Deserialized: {:?}", deserialized);
+    */
 }
