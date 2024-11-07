ce353b030228aa8a4de344c4b5b3615c67116163
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2024-11-07 18:00:19 +0100

    refactor(common): Added tests for methods in common lib.

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -17,3 +17,32 @@ pub fn serialize_machine(machine: &pw::messages::Machine) -> Vec<u8> {
 pub fn deserialize_machine(buf: &[u8]) -> Result<pw::messages::Machine, prost::DecodeError> {
     pw::messages::Machine::decode(buf)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn serialize_machine() {
+        let mut machine = pw::messages::Machine::default();
+        machine.mac = 12345678;
+        
+        let buffer = super::serialize_machine(&machine);
+
+        let expected: Vec<u8> = vec![8, 206, 194, 241, 5];
+
+        assert_eq!(buffer, expected);
+    }
+
+    #[test]
+    fn deserialize_machine() {
+        let buffer: Vec<u8> = vec![8, 206, 194, 241, 5];
+
+        let machine = super::deserialize_machine(&buffer);
+        
+        let mut expected = pw::messages::Machine::default();
+        expected.mac = 12345678;
+
+        assert_eq!(machine.unwrap(), expected);
+    }
+}
