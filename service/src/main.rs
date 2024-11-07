b892da603ffe42c5dfba60947184d0e179e016f9
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2024-11-07 18:29:23 +0100

    refactor(workspace): Moving things around, does not build.

--- service/src/main.rs
+++ service/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+
+#[tokio::main]
+async fn main() {
+    zenoh::init_log_from_env_or("error");
+
+    let config = zenoh::Config::default();
+    
+    let session = zenoh::open(config).await.unwrap();
+
+    
 }
