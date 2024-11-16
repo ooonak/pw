c236598fee4eb3d4cafa2fc6f6d7d4fa0bb2ca84
Casper Thorø Vium Pedersen <kanoo@posteo.de>
2024-11-16 13:42:25 +0100

    feat: Implementing liveliness. Client reports when services comes and goes.

--- README.md
+++ README.md
@@ -7,3 +7,17 @@
 I created the "Process Watcher tool" or pw as a little project while teaching myself Rust.
 
 The project consists of two binaries. A service (pw) running on a Linux target and a client (pwtui) that is running on your laptop. The pw service sends information about the machine and running processes. The pwtui client displayes this information. Like a distributed version of top.
+
+## Test-spin
+
+If you just wan't to build and start the service on your dev machine.
+
+```
+$ RUST_LOG=INFO cargo run --bin service
+```
+
+And in another terminal.
+
+```
+$ cargo run --bin service
+```
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,4 +1,20 @@
-use common::{deserialize_machine, BASE_KEY_EXPR, MACHINE_KEY_EXPR};
+use common::{deserialize_machine, BASE_KEY_EXPR, LIVELINESS_KEY_EXPR, MACHINE_KEY_EXPR};
+use zenoh::{config::ZenohId, sample::SampleKind};
+
+pub const GROUP_KEY_EXPR: &str = "grp1";
+
+async fn zenoh_info(session: &zenoh::Session) {
+    let info = session.info();
+    println!("zid: {}", info.zid().await);
+    println!(
+        "routers zid: {:?}",
+        info.routers_zid().await.collect::<Vec<ZenohId>>()
+    );
+    println!(
+        "peers zid: {:?}",
+        info.peers_zid().await.collect::<Vec<ZenohId>>()
+    );
+}
 
 #[tokio::main]
 async fn main() {
@@ -7,11 +23,16 @@ async fn main() {
     let config = zenoh::Config::default();
 
     let session = zenoh::open(config).await.unwrap();
+    zenoh_info(&session).await;
 
-    let key = format!("{}/*/{}/*", BASE_KEY_EXPR, MACHINE_KEY_EXPR);
+    let key_expr_machine = format!(
+        "{}/{}/{}/*",
+        BASE_KEY_EXPR,
+        GROUP_KEY_EXPR,
+        MACHINE_KEY_EXPR,
+    );
 
-    let replies = session.get(key).await.unwrap();
-
+    let replies = session.get(key_expr_machine).await.unwrap();
     while let Ok(reply) = replies.recv_async().await {
         match reply.result() {
             Ok(sample) => {
@@ -33,4 +54,35 @@ async fn main() {
             }
         }
     }
+
+    let key_expr_liveliness = format!(
+        "{}/{}/{}/**",
+        BASE_KEY_EXPR,
+        GROUP_KEY_EXPR,
+        LIVELINESS_KEY_EXPR
+    );
+
+    println!("Declaring Liveliness Subscriber on '{key_expr_liveliness}'...");
+    
+    let subscriber = session
+    .liveliness()
+    .declare_subscriber(&key_expr_liveliness)
+    .history(true)
+    .await
+    .unwrap();
+
+    println!("Press CTRL-C to quit...");
+    while let Ok(sample) = subscriber.recv_async().await {
+        match sample.kind() {
+            SampleKind::Put => println!(
+                ">> [LivelinessSubscriber] New alive token ('{}')",
+                sample.key_expr().as_str()
+            ),
+            SampleKind::Delete => println!(
+                ">> [LivelinessSubscriber] Dropped token ('{}')",
+                sample.key_expr().as_str()
+            ),
+        }
+    }
+
 }
